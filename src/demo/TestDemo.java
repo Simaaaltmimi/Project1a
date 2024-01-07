4743911bda2c7c52bda152a07fc1e7973a50c55a
simaa <simaa@ahmeds-mbp.lan>
2024-01-06 21:47:35 -0500

    add

---
+++ src/demo/TestDemo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class TestDemo {
+	public static void main(String[] args) {
+		System.out.println("Celine");
+		System.out.println("Simaa");
+	}
+public static void add(int a,int b) {
+	System.out.println(a+b);
+}
+}
